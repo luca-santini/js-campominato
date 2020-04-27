0b36d18e1c2fc2048f57cf64cf71917e8bafbab8
Luca Santini <lucasantinidev@gmail.com>
2020-04-27 17:21:02 +0200

    array genera mine

--- main.js
+++ main.js
@@ -4,20 +4,21 @@ function generaRandom(min, max) {
     var numero = Math.floor(Math.random() * (max - min + 1)) + min;
     return numero;
 }
-// attraverso un ciclo for, genero 16 numeri casuali, sfruttando la funzione generaRandom
-for (var i = 0; i < 16; i++) {
-    var numeroGenerato = generaRandom(1, 100);
-    console.log('numero random: ' + numeroGenerato);
+function generaMine() {
+    var listaMine = []
+    // attraverso un ciclo for, genero 16 numeri casuali, sfruttando la funzione generaRandom
+    for (var i = 0; i < 16; i++) {
+        var numeroGenerato = generaRandom(1, 100);
+        listaMine.push(numeroGenerato);
+        console.log('numero random: ' + numeroGenerato);
+    }
+    return listaMine;
 }
-// chiedere numero all'utente
+
+// chiedo un numero all'utente
 var numeroUtente = prompt('Inserisci un numero tra 1 e 100');
 console.log('numero utente: ' + numeroUtente);
+
 // verificare se il numero dell'utente corrisponde a quelli random
-var campoMinato;
-for (var i = 0; i < 16; i++) {
-    if (numeroUtente == numeroGenerato) {
-        // se è uguale stampa Colpito!
-        console.log('Colpito!');
-    // se è diverso stampa Sei Salvo!
-    } else console.log('Sei Salvo');
-}
+var lista = generaMine();
+console.log(lista);
