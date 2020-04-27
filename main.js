3eacbc3f7b9b2f24cbfefab89e09924fe1788d2f
Luca Santini <lucasantinidev@gmail.com>
2020-04-27 18:50:34 +0200

    soluzione di donato

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function generaRandom(min, max) {
     return numero;
 }
 function generaMine() {
-    var listaMine = []
+    var listaMine = [];
     // attraverso un ciclo for, genero 16 numeri casuali, sfruttando la funzione generaRandom
     for (var i = 0; i < 16; i++) {
         var numeroGenerato = generaRandom(1, 100);
@@ -15,10 +15,27 @@ function generaMine() {
     return listaMine;
 }
 
-// chiedo un numero all'utente
-var numeroUtente = prompt('Inserisci un numero tra 1 e 100');
-console.log('numero utente: ' + numeroUtente);
+function controlloMine(lst, numero) {
+    var trovato = false;
+    for (var i = 0; i < lst.length; i++) {
+        if(lst[i] == numero) {
+            trovato = true;
+        }
+    }
+    return trovato;
+}
 
 // verificare se il numero dell'utente corrisponde a quelli random
 var lista = generaMine();
 console.log(lista);
+
+var next = true;
+while (next) {
+    var numeroUtente = prompt('Inserisci un numero tra 1 e 100');
+    console.log(numeroUtente);
+
+    if (controlloMine(lista, numeroUtente)) {
+        next = false;
+        console.log('Hai Perso');
+    }
+}
